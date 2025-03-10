4373d6501a6a4aaba193ebf0465b200ded758c3b
kishan <kk1085035@gmail.com>
2025-03-10 09:30:00 +0530

    MOD

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/','/about','feed']
+    publicRoutes: ['/','/about']
 });
 
 export const config = {
